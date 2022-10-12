e700c5da216e993359159c915bf64b6ac24f3123
Samson Achiaga <sirhumble84@gmail.com>
2022-10-12 21:15:01 +0100

    Implement CustomDoublyLinkedList

--- src/CustomDoublyLinkedList.java
+++ src/CustomDoublyLinkedList.java
@@ -1,10 +1,10 @@
-public class CustomLinkedList<T> {
+public class CustomDoublyLinkedList<T> {
 
     private Node<T> head;
     private Node<T> tail;
     private int size;
 
-    public CustomLinkedList() {
+    public CustomDoublyLinkedList() {
         this.size = 0;
     }
 
@@ -34,17 +34,6 @@ public class CustomLinkedList<T> {
         return node;
     }
 
-    public void insert(T value) {
-        Node<T> node = new Node<>(value);
-        if (tail != null) {
-            tail.next = node;
-            tail = node;
-            size++;
-            return;
-        }
-        insertHead(value);
-    }
-
     public void insert(T value, int position) {
         if (position == 0) {
             insertHead(value);
@@ -52,10 +41,15 @@ public class CustomLinkedList<T> {
         } else if (position < 0 || position >= size) {
             System.out.println("Index is out of bound");
             return;
+        } else if (position == size - 1) {
+            insertTail(value);
+            return;
         } else {
-            Node<T> node = new Node<>(value);
-            node.next = get(position);
-            get(position - 1).next = node;
+            Node<T> next = get(position);
+            Node<T> prev = next.prev;
+            Node<T> node = new Node<>(value, next, prev);
+            prev.next = node;
+            next.prev = node;
         }
         size++;
     }
@@ -63,6 +57,8 @@ public class CustomLinkedList<T> {
     private void insertHead(T value) {
         Node<T> node = new Node<>(value);
         node.next = head;
+        if (head != null)
+            head.prev = node;
         head = node;
         if (tail == null)
             tail = node;
@@ -70,39 +66,46 @@ public class CustomLinkedList<T> {
     }
 
     private void insertTail(T value) {
-        Node<T> node = new Node<>();
-        node.value = value;
+        Node<T> node = new Node<>(value);
         tail.next = node;
+        node.prev = tail;
         tail = node;
         size++;
     }
 
     public void delete(int position) {
-        if (position < 0 || position >= size) {
+        if (position >= size) {
             System.out.println("Index out of bound");
             return;
         } else if (position == 0) {
             deleteHead();
             return;
+        } else if (position == size - 1) {
+            deleteTail();
+            return;
         } else {
             Node<T> node = get(position);
             Node<T> temp = get(position - 1);
             temp.next = node.next;
+            temp.next.prev = temp;
             node.next = null;
+            node.prev = null;
             if (position == size - 1)
                 tail = temp;
         }
         size--;
     }
 
-    public void delete() {
-        delete(size - 1);
-    }
-
     private void deleteHead() {
         head = head.next;
         if (head == null)
             tail = null;
+        size--;
+    }
+
+    private void deleteTail() {
+        tail.prev.next = null;
+        tail = tail.prev;
         size--;
     }
 
@@ -114,10 +117,6 @@ public class CustomLinkedList<T> {
             node = node.next;
         }
         return null;
-    }
-
-    public boolean contains(T value) {
-        return find(value) != null;
     }
 
     @Override
@@ -135,6 +134,7 @@ public class CustomLinkedList<T> {
     private class Node<E> {
         private E value;
         private Node<E> next;
+        private Node<E> prev;
 
         public Node() {
 
@@ -144,18 +144,23 @@ public class CustomLinkedList<T> {
             this.value = value;
         }
 
-        public Node(E value, Node<E> next) {
+        public Node(E value, Node<E> next, Node<E> prev) {
             this.value = value;
             this.next = next;
+            this.prev = prev;
         }
 
         @Override
         public String toString() {
             String next;
+            String prev;
             if (this.next == null)
                 next = null;
             else next = String.valueOf(this.next.value);
-            return "Value: " + value + " Next: " + next;
+            if (this.prev == null)
+                prev = null;
+            else prev = String.valueOf(this.prev.value);
+            return "Value: " + value + " Next: " + next + " Prev: " + prev;
         }
     }
 }
